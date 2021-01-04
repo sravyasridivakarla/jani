d32bab1433fd78d071d4759894d899aca831af24
sravyasridivakarla <32864008+sravyasridivakarla@users.noreply.github.com>
2021-01-03 17:09:36 -0800

    Add files via upload

---
+++ perlin/sketch.js
@@ -0,0 +1,71 @@
+var cols, rows;
+var scl = 20;
+var w = 1400;
+var h = 1000;
+
+var song;
+var button;
+var amp;
+
+var flying = 0;
+
+var terrain = [];
+
+function setup() {
+  createCanvas(600, 600, WEBGL);
+  cols = w / scl;
+  rows = h / scl;
+  song = loadSound('perlin.mp3', loaded);
+  amp = new p5.Amplitude();
+
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; //specify a default value for now
+    }
+  }
+}
+function loaded() {
+  button = createButton('play');
+  button.mousePressed(togglePlaying);
+}
+
+function draw() {
+
+  var vol = amp.getLevel();
+
+  flying -= 0.1;
+  var yoff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(vol, 0, 1, -100, 100);
+      xoff += 0.2;
+    }
+    yoff += 0.2;
+  }
+
+  background(0);
+  translate(0, 50);
+  rotateX(PI / 3);
+  fill(200, 200, 200, 50);
+  translate(-w / 2, -h / 2);
+  for (var y = 0; y < rows - 1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
+    }
+    endShape();
+  }
+}
+function togglePlaying() {
+  if (!song.isPlaying()) {
+    song.play();
+    song.setVolume(0.3);
+    button.html('pause');
+  } else {
+    song.stop();
+    button.html('play');
+  }
+}
